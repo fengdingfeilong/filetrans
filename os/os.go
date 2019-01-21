7bae4ec0553d901d52cf72c11184dbfcec0ab82c
fengdingfeilong <1374084580@qq.com>
2019-01-21 16:03:20 +0800

    Update os.go

--- os/os.go
+++ os/os.go
@@ -1,6 +1,6 @@
 package os
 
-// OS 系统类型
+// OS system type
 type OS int
 
 const (
@@ -8,14 +8,14 @@ const (
 	Win OS = iota
 	// Win7 windows7
 	Win7
-	// Win10 windows
+	// Win10 windows10
 	Win10
-	// MacOS darwin 苹果PC系统
+	// MacOS
 	MacOS
-	// iOS 苹果手机系统
-	iOS //TODO: 小写，不可导出，包外不可用 建议修改为 'IOS'
+	// IOS iphone syetem
+	IOS
 	// Android 安卓手机系统
 	Android
-	// linux linux // TODO: 同 iOS
-	linux
+	// Linux
+	Linux
 )
